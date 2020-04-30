31c0487b7ae70974f610d34503fd9c56c9e862d5
colevandersWands <18554853+colevandersWands@users.noreply.github.com>
2020-04-30 22:37:00 +0200

    better-boilerplate

--- 3-write-the-fetch/challenge-4.js
+++ 3-write-the-fetch/challenge-4.js
@@ -6,41 +6,46 @@ const assert = require('assert');
 
 // declare constants
 const START = Date.now();
-const REPORT_FILE = path.basename(__filename).replace('.js', '-report.txt');
+const REPORT_FILE = __dirname + '/' + path.basename(__filename).replace('.js', '-report.txt');
 
 // define logging function
 const log = (msg) => {
   const now = `${Date.now() - START} ms: `;
   console.log(now + msg);
-  const writeableMsg = !msg ? msg
-    : typeof msg === 'string'
-      ? msg.split(__dirname).join(' [ ... ] ')
-        .split('[31m').join('').split('[32m').join('').split('[39m').join('')
-      : JSON.stringify(msg);
-  fs.appendFileSync('./' + REPORT_FILE, now + writeableMsg + '\n');
+  if (typeof msg === 'string') {
+    const cleanedString = msg
+      // remove special characters used to print assertion colors in terminal
+      .replace(/\[31m|\[32m|\[39m/g, '')
+      // remove the file path from error messages for privacy and readability
+      .replace(new RegExp(__dirname, "g"), ' [ ... ] ');
+    fs.appendFileSync(REPORT_FILE, now + cleanedString + '\n');
+  } else {
+    const stringifiedMsg = JSON.stringify(msg);
+    fs.appendFileSync(REPORT_FILE, now + stringifiedMsg + '\n');
+  };
 };
 
-// last-resort error logging
-process.on('uncaughtException', (err) => log(err));
-process.on('unhandledRejection', (err) => log(err));
+// log when a user forces the script to exit
+process.on('SIGINT', function onSIGINT() {
+  log('Ctrl-C');
+  process.exit(2);
+});
+
+// log uncaught errors
+const handleError = (err) => {
+  log(err);
+  process.exit(1);
+};
+process.on('uncaughtException', handleError);
+process.on('unhandledRejection', handleError);
 
 // (re)initialize report file
 fs.writeFileSync(REPORT_FILE, '');
 log((new Date()).toLocaleString());
 
 
-// begin main script
+// --- begin main script ---
 
 // the pokemon who's previous evolution is "Meowth"
 
 
-
-const dotDotDot = setInterval(() => {
-  log('...');
-}, 100);
-
-
-process.on('SIGINT', function onSIGINT() {
-  log('Ctrl-C');
-  process.exit(2);
-});
